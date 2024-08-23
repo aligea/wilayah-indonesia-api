27716084385dcc7ab3d451948f60306fe4af8033
Ali Bangun Gea <alibangungea@gmail.com>
2024-08-23 21:24:31 +0700

    finishing routes

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const services = require("./services");
+const controllers = require('./controllers');
 
 router.get("/", (req, res) => {
   res.send("API Documentation");
 });
 
-router.get("/provinces", async (req, res) => {
-  let Provinces = await services.province.getAll();
-  res
-    .status(200)
-    .send({ response: { status: 200, message: "OK" }, data: Provinces });
-});
-router.get("/province/:provinceId", async (req, res) => {
-  const {
-    params: { provinceId },
-  } = req;
-  let Province = await services.province.getOne(provinceId);
-
-  res
-    .status(200)
-    .send({ response: { status: 200, message: "OK" }, data: Province });
-});
+router.get("/provinces", controllers.ProvinceController.getAllProvince);
+router.get("/province/:provinceId", controllers.ProvinceController.getProvinceByID);
+router.get("/regencies/:provinceId", controllers.RegencyController.getAllRegenciesInProvince);
+router.get("/regency/:regencyID", controllers.RegencyController.getRegencyByID);
+router.get("/districts/:regencyID", controllers.DistrictController.getDistrictsInRegency);
+router.get("/district/:districtID", controllers.DistrictController.getDistrictByID);
+router.get("/villages/:districtID", controllers.VillageController.getVillagesInDistrict);
+router.get("/village/:villageID", controllers.VillageController.getVillageByID);
+router.get("/search/:query", controllers.SearchController.getResultByQuery);
 
 module.exports = router;
